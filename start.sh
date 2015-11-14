ce42b5381a0a38db9ca9d39cf6b17a7860036715
Marcin <marcin@jozwikowski.pl>
2015-11-14 19:40:38 +0100

    Instructions after start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,15 @@ sudo rm dev_mach/ -r -f
 git clone https://github.com/marcin-jozwikowski/devMachine.git dev_mach
 sudo find ./dev_mach/. -type f -name '*.sh' -exec chmod o+x {} \;
 sudo rm start.sh -f
+echo ""
+echo ""
+echo "******************************************************"
+echo "*                                                    *"
+echo "*   Now run the following commands                   *"
+echo "*                                                    *"
+echo "*   cd dev_mach/                                     *"
+echo "*   bash 1_init.sh                                   *"
+echo "*                                                    *"
+echo "******************************************************"
+echo ""
+echo ""
